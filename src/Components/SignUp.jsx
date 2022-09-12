8980256e4384777306204db8c34c22161e3e935d
עידו תשובה <idoteshuva@h-MacBook-Pro-sl-ydw.local>
2022-09-12 13:14:10 +0300

    signuplastformik

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,56 +1,50 @@
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import { useState } from "react";
 import { basicSchema } from "./schemas/indexBasicForm";
 import "./sign-up.css";
 import { Button, TextField } from "@mui/material";
 import { regisNotOpened, loginOpened} from "../counterSlice";
 import { useDispatch } from "react-redux";
-import * as Yup from "yup"
 
 const SignUp = () => {
   const handleModalClose = () => {
     dispatch(regisNotOpened());
     dispatch(loginOpened());}
 const dispatch = useDispatch();
-
-const schema = Yup.object().shape({
-  email: Yup.string().required("Please enter email").email("Not a valid email"),
-  username: Yup.string().required("please enter username"),
-  age: Yup.number().positive().integer().required("please enter your age"),
-  password: Yup.string().min(8).required("please enter password"),
-  confirmpassword: Yup.string().oneOf(Yup.ref("password"), null).required()
-})
-  return (
-    <div >
-    <Formik
-    initialValues={{
+  const onSubmit = async (actions) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    actions.resetForm();
+  };
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
       email: "",
       username: "",
       age: "",
       password: "",
-      confirmpassword: ""
-    }}
-    onSubmit={(values) => {
-      //your code
-    }}
-    validationSchema = {schema}
-    >
-      {({
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        values,
-        errors,
-        touched
-      })=> (
+      confirmPassword: "",
+    },
     
+    validationSchema: basicSchema 
+  });
+
+  return (
+    <div >
+
     <form  className="main-signup" onSubmit={handleSubmit} autoComplete="off">
 
       
       <label htmlFor="email">Email</label>
       <TextField
         size="small"
-        id="email"
+        id="Email"
         type="email"
         placeholder="enter your email"
         value={values.email}
@@ -58,10 +52,10 @@ const schema = Yup.object().shape({
         onBlur={handleBlur}
         className={errors.email && touched.email ? "input-error" : ""}
       />
-      <p>{errors.email && touched.email && errors.email}</p>
+      {errors.email && touched.email && <p className="error">{errors.email}</p>}
       
 
-      <label htmlFor="username">username</label>
+      <label htmlFor="username">Username</label>
       <TextField
         size="small"
         id="username"
@@ -72,10 +66,10 @@ const schema = Yup.object().shape({
         onBlur={handleBlur}
         className={errors.username && touched.username ? "input-error" : ""}
       />
-      <p>{errors.username && touched.username && errors.username}</p>
+      {errors.username && touched.username && <p className="error">{errors.username}</p>}
 
 
-      <label htmlFor="age">age</label>
+      <label htmlFor="age">Age</label>
       <TextField
         size="small"
         id="age"
@@ -86,11 +80,11 @@ const schema = Yup.object().shape({
         onBlur={handleBlur}
         className={errors.age && touched.age && errors.age}
       />
-      <p>{errors.age && touched.age && errors.age}</p>
+      {errors.age && touched.age && <p className="error">{errors.age}</p>}
 
 
 
-      <label htmlFor="password">password</label>    
+      <label htmlFor="password">Password</label>    
       <TextField
         size="small"
         id="password"
@@ -99,12 +93,14 @@ const schema = Yup.object().shape({
         value={values.password}
         onChange={handleChange}
         onBlur={handleBlur}
+        className={errors.password && touched.password ? "input-error" : ""}
       />
-      <p>{errors.password && touched.password && errors.password}</p>
+      {errors.password && touched.password && (
+        <p className="error">{errors.password}</p>
+      )}
 
 
-
-      <label htmlFor="confirmPassword">confirm password</label>
+      <label htmlFor="confirm Password">Confirm password</label>
       <TextField
         size="small"
         id="confirmPassword"
@@ -113,18 +109,20 @@ const schema = Yup.object().shape({
         value={values.confirmPassword}
         onChange={handleChange}
         onBlur={handleBlur}
+        className={
+          errors.confirmPassword && touched.confirmPassword ? "input-error" : ""
+        }
       />
-      <p>{errors.confirmpassword && touched.confirmpassword && errors.confirmpassword}</p>
+      {errors.confirmPassword && touched.confirmPassword && (
+        <p className="error">{errors.confirmPassword}</p>
+      )}
 
 
-
-      <Button type="submit" className="signup-button" variant="contained">sign up</Button>
+      <Button type="submit" className="signup-button" disabled={isSubmitting} variant="contained">sign up</Button>
       <br />
       <Button className="already-button" onClick={handleModalClose} variant="outlined" >Already have an account?</Button>
     
     </form>
-    )}
-    </Formik>
     </div>
   );
 };
